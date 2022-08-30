5f4aa405dcff1495896cf87207cf32839da025d3
Rounak Biswas <rounakbiswas1002@gmail.com>
2022-08-30 13:20:09 +0530

    initial commit

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,15 @@
+import spinner from "./assets/spinner.gif"
+
+function Spinner() {
+	return (
+		<div className='w-100 mt-200'>
+			<img
+				src={spinner}
+				alt='Loading...'
+				width={180}
+				className='text-center mx-auto'
+			/>
+		</div>
+	)
+}
+export default Spinner
